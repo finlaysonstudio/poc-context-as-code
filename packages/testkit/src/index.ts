1c73617ab7673fada1f70a2ddb9be68969ce6ba7
Adam Finlayson <amf@adamfinlayson.com>
2025-04-11 08:06:56 -0500

    chore: config: type in index

--- packages/testkit/src/index.ts
+++ packages/testkit/src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /**
  * @jaypie/testkit
  * Testing utilities for Jaypie projects
@@ -6,3 +7,18 @@
 import { toBeTaco } from "./matchers/toBeTaco";
 
 export { toBeTaco };
+
+import "@vitest/expect";
+
+interface CustomMatchers<R = any> {
+  toBeTaco(): R;
+}
+
+declare module "@vitest/expect" {
+  interface Assertion<T = any> extends CustomMatchers<T> {
+    toBe(expected: T): void;
+  }
+  interface AsymmetricMatchersContaining extends CustomMatchers {
+    toBe(expected: unknown): unknown;
+  }
+}
